71280e7eba2b2a0883a9dac703932803cce8e45a
Maxim Dang <dang.maxim2811@gmail.com>
2024-07-17 13:04:58 +0300

    first commit

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"Rest1/internal/config"
+	"Rest1/internal/http-server/middleware/logger"
+	"Rest1/internal/lib/logger/sl"
+	"Rest1/internal/storage/sqlite"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener server...", slog.String("env", cfg.Env))
+	log.Debug("debug message")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+
+	_ = storage
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID, middleware.Logger, logger.New(log))
+	router.Use(middleware.Recoverer, middleware.URLFormat)
+
+	// TODO:run server:
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
